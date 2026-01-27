ba4fb92c78ad4f71eeda10ade9d9f67ad400deb9
aleenghanayim132-ops <aleenghanayim132@gmail.com>
2026-01-27 21:15:44 +0200

    Implement image loading and edge detection functions

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,20 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    image = Image.open(path)
+    image_array = np.array(image)
+    return image_array
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    image_array = Image.open(image).convert('L')
+    image_gray = np.array(image_array)
+    filterX = np.array([[-1,0,1],
+                        [-2,0,2],
+                        [-1,0,1]])
+    filterY = np.array([[1,2,1],
+                        [0,0,0],
+                        [-1,-2,-1]])
+    filtered_imageX = convolve2d(image_gray,filterX, mode='same' , boundary='symm')
+    filtered_imageY = convolve2d(image_gray,filterY, mode='same' , boundary='symm') 
+    filtered_image = np.sqrt(filtered_imageX**2 + filtered_imageY**2)
+    return filtered_image
