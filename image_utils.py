caa458a0c864b0d9fc37f7ac5b37d66e1af5f257
aleenghanayim132-ops <aleenghanayim132@gmail.com>
2026-01-30 02:43:46 +0200

    Refactor image loading and edge detection functions

--- image_utils.py
+++ image_utils.py
@@ -3,20 +3,16 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    image = Image.open(path)
-    image_array = np.array(image)
-    return image_array
+    return np.array(Image.open(path).convert('RGB'))
 
-def edge_detection(image):
-    image_array = Image.open(image).convert('L')
-    image_gray = np.array(image_array)
-    filterX = np.array([[-1,0,1],
-                        [-2,0,2],
-                        [-1,0,1]])
-    filterY = np.array([[1,2,1],
-                        [0,0,0],
-                        [-1,-2,-1]])
-    filtered_imageX = convolve2d(image_gray,filterX, mode='same' , boundary='symm')
-    filtered_imageY = convolve2d(image_gray,filterY, mode='same' , boundary='symm') 
-    filtered_image = np.sqrt(filtered_imageX**2 + filtered_imageY**2)
-    return filtered_image
+def edge_detection(image_array):
+    weights = np.array([0.2989, 0.5870, 0.1140])
+    image_gray = np.dot(image_array[..., :3], weights)
+
+    filterX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    filterY = np.array([[ 1, 2, 1], [ 0, 0, 0], [-1,-2,-1]])
+
+    gx = convolve2d(image_gray, filterX, mode='same', boundary='symm')
+    gy = convolve2d(image_gray, filterY, mode='same', boundary='symm') 
+  
+    return np.sqrt(gx**2 + gy**2)
